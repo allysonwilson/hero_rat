693a0960e78d04a2d2d3bfa71146279a73a54228
Allyson Gayle Wilson <allysonmacwilson@gmail.com>
2017-10-13 15:26:30 +0100

    hero has favourite food

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,12 +1,12 @@
 var assert = require("assert");
-var Hero = require("../hero");
+var Hero = require("../hero.js");
 
 beforeEach(function () {
   wonderWoman = new Hero("Diana Prince", 100, "cheese burger");
 });
 
-describe("Hero", function(){
+describe("Hero", function() {
   it("should have a favouriteFood", function() {
-    assert.strictEqual(womderWoman.favouriteFood, "cheese burger");
+    assert.strictEqual(wonderWoman.favouriteFood, "cheese burger");
   });
-})
+});
